50410d55dc412b17c16d3e6ac20d619cda1a0f56
Lindo-code <sdrowvieli1@gmail.com>
2022-02-26 20:31:31 +0200

    changed colors, added errors and added logo.

--- Calculator.js
+++ Calculator.js
@@ -1,16 +1,24 @@
 class Tipcalculator {
   constructor() {
-    this.bill = 100;
-    this.persons = 5;
-    this.tip = 15;
+    this.bill = 0;
+    this.persons = 0;
+    this.tip = 0;
     this.tipAmmount = 0; //per person
     this.total = 0; //per person
   }
   billing(value1) {
+    const element0 = document.getElementById("error0");
     this.bill = value1;
+    if (element0.style.display === "block") {
+      element0.style.display = "none";
+    }
   }
   persons_(value3) {
+    const element = document.getElementById("error1");
     this.persons = value3;
+    if (element.style.display === "block") {
+      element.style.display = "none";
+    }
   }
   calcTipPP(num) {
     this.tip = num;
@@ -18,17 +26,35 @@ class Tipcalculator {
     this.tipAmmount = this.tipAmmount/this.persons;
     this.tipAmmount = Number(this.tipAmmount.toString().replace(/(?<=\.\d\d).*/, ""));
     this.total = this.calcTotalPP(this.tipAmmount);
+    const element = document.getElementById("error1");
+    const element0 = document.getElementById("error0");
+
+    if (this.bill === 0 || this.bill === NaN || this.bill === "") {
+      element0.style.display = "block";
+    }
+    if (this.persons === 0 || this.persons === NaN || this.persons === "") {
+      element.style.display = "block";
+    } else {
+      if (element0.style.display === "block" || element.style.display === "block") {
+        element0.style.display = "none";
+        element.style.display = "none";
+      }
+      const element1 = document.getElementById('tAResult');
+      const element2 = document.getElementById('tPResult');
+      element1.innerText = this.tipAmmount;
+      element2.innerText = this.total;
+    }
   }
   calcTotalPP(roundToTwo) {
     this.total = (this.bill/this.persons) + roundToTwo;
     this.total = Number(this.total.toString().replace(/(?<=\.\d\d).*/, ""));
     return this.total;
   }
-  calculate() {
+  reset() {
     const element1 = document.getElementById('tAResult');
     const element2 = document.getElementById('tPResult');
-    element1.innerText = this.tipAmmount;
-    element2.innerText = this.total;
+    element1.innerText = 0;
+    element2.innerText = 0;
   }
 }
 const calculator = new Tipcalculator();
